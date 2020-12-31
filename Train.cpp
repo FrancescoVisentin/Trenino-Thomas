0f36b9a86f0a67bacb764ebf09a64b18ec3dbaea
FrancescoVisentin <74708171+FrancescoVisentin@users.noreply.github.com>
2020-12-31 19:10:45 +0100

    Treno Modificato

--- Train.cpp
+++ Train.cpp
@@ -27,29 +27,73 @@ Train::Train(bool origin, int train_type, vector<int> arrival_times, vector<bool
     delay = 0;              //ritardo iniziale nullo
     
 
+    /*
+    l'idea è la seguente:
+    voglio che il vettore distances contenga le distanze tra stazioni che sono effettivamente corrette.
+    Se una stazione è sbagliata la rimuovo quindi la rimuovo da tutti e tre i vettori. VIENE QUINDI GIA' RIMOSSO ANCHE 
+    IL CORRISPETTIVO TEMPO DI ARRIVO DA ARRIVAL_TIMES.
 
+    infatti:
+    -nei treni regionali che fermano in tutte le stazioni gli indici dei corrispettivi dati nei tre vettori concidono ed,
+     al massimo, se arrival_times è più corto degli altri, non serve nemmeno rimuovere la stazione rotta da li.
 
-    vector<int> distances;          //questo vettore conterrà tutte le distanze tra le varie stazioni
+     -nei treni veloci può essere presente un offset. questo offeset è dato dal numero di stazioni locali che si trovano all'interno
+      (rispettivamente) di stations_distances ed stations_type. io conto quindi quante stazioni secondarie (non nulle) trovo e quello sarà
+      l'offset che mi permette di lavorare su arrival_times correttamente
+
+    Ora dovresti avere:
+     -stations_distances ed stations_type corretti e tutti con stazioni validi.
+     -arrival_times con tempi relativi a stazioni valide MA NON CORRETTI.
+
+    Dovrebbe bastar aggiustare check_arrival_times compito che le affido volentieri.
+    BUON ANNO, spero che il tutto abbia senso, dimmi se ho scritto stronzate <3
+
+    */
+    vector<int> distances;     //SPOSTARE MEMBRO     //questo vettore conterrà tutte le distanze tra le varie stazioni
     if(train_type == 1)
     {
-        for(int i=0; i<stations_distances.size()-1; i++)
-        {
-            int offset1 = 1;
-            while((i+offset1 < stations_distances.size()) && (station_distances.at(i+offset1) < 0))
+        //rimuovo eventuali stazioni smarze.
+        for(int i = 0; i < stations_distances.size(); i++) // lo passo tutto perchè c++ non implementa nemmeno una cazzo di
+        {                                                  // ricerca binaria che restetuisce l'indirizzo e non ho palle di scriverla io
+            if(stations_distances[i] == -1)
             {
-                offset1++;
+                stations_distances.erase(stations_distances.begin() + i);
+                stations_type.erase(stations_type.begin() + i);
+
+                if(i < arrival_times.size())
+                    arrival_times.erase(arrival_times.begin() + i);
             }
-            distances.push_back(stations_distances.at(i+offset1)-stations_distances.at(i));
         }
+    
+        //distances tra tutte le stazioni.
+        for(int i=0; i<stations_distances.size()-1; i++)
+            distances.push_back(stations_distances.at(i+1)-stations_distances.at(i));
     }
     else
     {
+        int skipped_stations {0}; //è il numero di stazioni secondarie che rappresenta l'offset tra il vettore arrival_times ed i vettori stations_distances/times
+        for(int i = 0; i < stations_distances.size(); i++)
+        {
+            if(stations_type[i] == 1 && stations_distances[i] != -1) skipped_stations++; //rappresenta l'offset tra il vet
+
+            if(stations_distances[i] == -1)
+            {
+                if(stations_type[i] == 0 && (i-skipped_stations) < arrival_times.size())
+                    arrival_times.erase(arrival_times.begin()+ (i-skipped_stations));
+                
+                
+                stations_distances.erase(stations_distances.begin() + i);
+                stations_type.erase(stations_type.begin() + i);
+            }
+        }
+
+        //QUI NON HO PRATICAMENTE TOCCATO NIENTE, ho solo rimosso condizione ridondante.
         for(int i=0; i<stations_distances.size()-1; i++)
         {
             if(stations_type.at(i) == 0)
             {
                 int offset2 = 1;
-                while(((i+offset2) < stations_type.size()) && (station_distances.at(i+offset2) < 0) && (stations_type.at(i+offset2) != 0))
+                while(((i+offset2) < stations_type.size()) && (stations_type.at(i+offset2) != 0))
                 {
                     offset2++;
                 }
